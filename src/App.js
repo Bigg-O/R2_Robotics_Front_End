df7ca9fbe9d9bbab26d48d4f2c6bece4880fc075
wookeun song <wookeun.s@gmail.com>
2022-11-13 01:47:25 -0500

    log in and sighn up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from "./Components/Forms/Login"
+import Signup from "./Components/Forms/Signup"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {  }
+  render() { 
+    return ( 
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+
+
+
+        </Routes>
+      </Router>
+     );
+  }
 }
-
+ 
 export default App;
---
+++ src/Components/Forms/Signup.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import "../css/Signup.css";
+import Logo from "../../Images/Logo.png";
+import { Container, Image } from "react-bootstrap";
+import { Link, Redirect } from "react-router-dom";
+import SignupForm from "./SignupForm";
+import axios from "axios";
+// import history from "../../history";
+// const HEROKU_URL = "https://dareme-server.herokuapp.com/";
+
+class Signup extends Component {
+    render() { 
+        return (
+            <Container fluid className="signup-container">
+                <Image className="login-logo" src={Logo} fluid />
+
+                <h1 className="signup-desc">Create an Account!</h1>
+
+                <SignupForm onSubmit={this.handleSubmission} />
+
+                <p className="b2-login">
+                    NVM I already have an account, back to{" "}
+                    <Link className="login-link" to="/login">
+                    Log In
+                    </Link>
+                </p>
+            </Container>
+        );
+    }
+}
+ 
+export default Signup;
